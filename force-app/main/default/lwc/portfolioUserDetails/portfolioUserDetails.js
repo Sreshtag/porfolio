18e7814c753d87b9a125e6f1b49a7b5a7ca1e042
Sreshta Gollapally <sreshtag11@gmail.com>
2025-01-06 22:49:38 +0530

    badge and button

--- force-app/main/default/lwc/portfolioUserDetails/portfolioUserDetails.js
+++ force-app/main/default/lwc/portfolioUserDetails/portfolioUserDetails.js
@@ -3,7 +3,7 @@ import { LightningElement,api } from 'lwc';
 export default class PortfolioUserDetails extends LightningElement {
     @api recordId
     @api objectName
-    downloaResume(){
-        console.log("details from parent comp",this.recordId,this.objectName)
-    }
+    // downloaResume(){
+    //     console.log("clicked download resume")
+    // }
 }
